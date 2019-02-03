872b8a2305ea5378f45293a4de8e506a3c232999
Daniel Frank <daniel.frank@lin-digital.com>
2019-02-03 01:28:57 -0800

    Remove dbugging

--- server/server.js
+++ server/server.js
@@ -69,14 +69,12 @@ app.patch('/todos/:id', (req,res) => {
         return res.status(404).send();
     }
     var body = _.pick(req.body, ['text', 'completed']);
-    console.log(body);
     if(_.isBoolean(body.completed) && body.completed) {
         body.completedAt = new Date().getTime();
     } else {
         body.completed = false;
         body.completedAt = null;
     }
-    console.log(body);
     Todo.findByIdAndUpdate(id, {$set: body}, {new: true}).then((todo) =>{
         if(!todo) {
             return res.status(404).send(todo);
