8937da0e3b0fe376009e86649a189cfd8d2ca187
Daniel Frank <daniel.frank@lin-digital.com>
2019-02-02 23:24:11 -0800

    Add test cases for GET /todos/:id

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -1,12 +1,15 @@
 const expect = require('expect');
 const request = require('supertest');
+const {ObjectID} = require('mongodb');
 
 const {app} = require('./../server');
 const {Todo} = require('./../models/todo');
 
 const todos = [{
+    _id: new ObjectID,
     text: 'First test todo'
 },{
+    _id: new ObjectID,
     text: 'Second test todo'
 }];
 
@@ -66,4 +69,32 @@ describe('GET /todos', () => {
         })
         .end(done);
     });
+});
+
+describe('GET /todos/:id', () => {
+    it('should return todo doc',(done) => {
+        request(app)
+        .get(`/todos/${todos[0]._id.toHexString()}`)
+        .expect(200)
+        .expect((res) => {
+            expect(res.body.todo.text).toBe(todos[0].text)
+        })
+
+        .end(done);
+    });
+    it('should return 404 if todo doc not found',(done) => {
+        var id = new ObjectID().toHexString();
+        request(app)
+        .get(`/todos/${id}`)
+        .expect(404)
+        .end(done);
+    });
+    it('should return 404 if not valid ObjectID',(done) => {
+        var id = new ObjectID().toHexString() + '11';
+        request(app)
+        .get(`/todos/${id}`)
+        .expect(404)
+        .end(done);
+    });
+
 });
