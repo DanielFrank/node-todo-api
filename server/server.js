3c7b757da1710ec397de00a055b11a82c42f246c
Daniel Frank <daniel.frank@lin-digital.com>
2019-02-03 01:28:08 -0800

    Add PATCH /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
-var express = require('express');
-var bodyParser = require('body-parser');
+const _ = require('lodash');
+const express = require('express');
+const bodyParser = require('body-parser');
 const {ObjectID} = require('mongodb');
 
 var {mongoose} = require('./db/mongoose');
@@ -61,6 +62,30 @@ app.delete('/todos/:id', (req,res) => {
     }).catch((e) => res.status(400).send())
 });
 
+//PATCH /todos/12345
+app.patch('/todos/:id', (req,res) => {
+    var id = req.params.id;
+    if (!ObjectID.isValid(id)) {
+        return res.status(404).send();
+    }
+    var body = _.pick(req.body, ['text', 'completed']);
+    console.log(body);
+    if(_.isBoolean(body.completed) && body.completed) {
+        body.completedAt = new Date().getTime();
+    } else {
+        body.completed = false;
+        body.completedAt = null;
+    }
+    console.log(body);
+    Todo.findByIdAndUpdate(id, {$set: body}, {new: true}).then((todo) =>{
+        if(!todo) {
+            return res.status(404).send(todo);
+        }
+        res.send({todo});       
+    }).catch((e) => {
+        res.status(400).send();
+    });
+});
 
 app.listen(port, () => {
     console.log('Started on port 3000');
